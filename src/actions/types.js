9cf9481d2e46491d6efabf41b1da8a9ccee26cbe
Deep Singh <77362196+DeepCodeTech@users.noreply.github.com>
2024-04-27 18:20:27 +0530

    Delete src directory

--- src/actions/types.js
+++
@@ -1,6 +0,0 @@
-export const GET_WEATHER = 'GET_WEATHER';
-export const SET_LOADING = 'SET_LOADING';
-export const SET_ERROR = 'SET_ERROR';
-export const CLEAR_WEATHER_DATA = 'CLEAR_WEATHER_DATA';
-export const CLEAR_ERROR = 'CLEAR_ERROR';
-export const SET_SUGGESTIONS = 'SET_SUGGESTIONS';
